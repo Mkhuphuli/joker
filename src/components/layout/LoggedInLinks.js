aa0e0c95f671ecba59a32de4f27a06894f9ad37a
Mkhuphuli <32544431+Mkhuphuli@users.noreply.github.com>
2019-07-07 00:23:09 +0200

    formatted navigation bar links

--- src/components/layout/LoggedInLinks.js
+++ src/components/layout/LoggedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const LoggedInLinks=()=>{
     return (
         <ul className="navbar-links-right">
-            <li><NavLink to='/joke' className='crackJokeLink'>Crack Joke</NavLink></li>
+            <li><NavLink to='/joke' className='crackJokeLink '>Crack Joke</NavLink></li> |
             <li><NavLink to='/register' className='registerLink'>Register</NavLink></li>
         </ul>
     )
--- src/components/layout/LoggedOutLinks.js
+++ src/components/layout/LoggedOutLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const LoggedOutLinks=()=>{
     return (
         <ul className="navbar-links-right">
-            <li><NavLink to='/login' className='loginLink'>Login</NavLink></li>
+            <li><NavLink to='/login' className='loginLink left-position'>Login</NavLink></li> |
             <li><NavLink to='/register' className='registerLink'>Register</NavLink></li>
         </ul>
     )
