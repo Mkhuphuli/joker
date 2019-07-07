2d72a6f8041f0e48ac3fee5e777fe4281c151fe4
Mkhuphuli <32544431+Mkhuphuli@users.noreply.github.com>
2019-07-07 11:32:58 +0200

    linked the play-button to https://icanhazdadjoke.com/ api

--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
+import axios from 'axios'; 
 
 
 class LoggedIn extends Component{
     state={
         joke:"JOKE TEXT HERE"
     }
+    getJoke=()=>{
+        axios({
+            url:"https://icanhazdadjoke.com/",
+            method:'get',
+	    	headers: {
+                'accept': 'application/json'
+            }
+        }).then(response =>{
+            this.setState({
+                joke:response.data.joke
+            })
+        })
+
+
+        
+    }
     render(){
         return (
             <div className="jokes-container">
                 {this.state.joke}
                 <br/>
-                <img className='play-button' src={require("../play-button.png")} alt="Play"/>
+                <img className='play-button' src={require("../play-button.png")} alt="Play" onClick={this.getJoke}/>
             </div>
             
         )
